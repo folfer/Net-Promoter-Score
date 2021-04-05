2ae1c991761daa09102e3134c8d1b98c7325f4ad
folfer <victorcalazansramos@gmail.com>
2021-04-04 23:09:08 -0300

    [Code] creating surveys routes and models

---
+++ src/controllers/SendMailController.ts
@@ -0,0 +1,19 @@
+// import { Response, Request } from "express";
+// import { getCustomRepository } from "typeorm";
+// import { UsersRepository } from '../repositories/UsersRepository';
+// import { SurveysRepository } from '../repositories/SurveysRepository';
+// import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
+
+// class SendMailController {
+
+//   async execute(request: Request, response: Response) {
+//      const { email, survey_id } = request.body;
+
+//      const usersRepository = getCustomRepository(UsersRepository);
+//      const surveysRepository = getCustomRepository(SurveysRepository);
+//      const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+
+//   }
+// }
+
+// export { SendMailController };
