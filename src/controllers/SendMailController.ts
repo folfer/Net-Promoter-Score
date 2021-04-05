99d3755fee12f5f0e8f6f94d38c772e14d22fcf4
folfer <victorcalazansramos@gmail.com>
2021-04-04 23:54:29 -0300

    [Code] tests init

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -1,19 +1,18 @@
-// import { Response, Request } from "express";
-// import { getCustomRepository } from "typeorm";
-// import { UsersRepository } from '../repositories/UsersRepository';
-// import { SurveysRepository } from '../repositories/SurveysRepository';
-// import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
+import { Response, Request } from "express";
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from '../repositories/UsersRepository';
+import { SurveysRepository } from '../repositories/SurveysRepository';
+import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
 
-// class SendMailController {
+class SendMailController {
+  async execute(request: Request, response: Response) {
+     const { email, survey_id } = request.body;
 
-//   async execute(request: Request, response: Response) {
-//      const { email, survey_id } = request.body;
+     const usersRepository = getCustomRepository(UsersRepository);
+     const surveysRepository = getCustomRepository(SurveysRepository);
+     const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
 
-//      const usersRepository = getCustomRepository(UsersRepository);
-//      const surveysRepository = getCustomRepository(SurveysRepository);
-//      const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+  }
+}
 
-//   }
-// }
-
-// export { SendMailController };
+export { SendMailController };
